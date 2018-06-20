3c8f7beb99d23986964012cc3f68a1affe6b43b8
Nicolai Vasquez <nrvasquez@gmail.com>
2018-06-20 23:26:55 +0100

    update readme and quick wireframe of ui

--- ui/src/App.js
+++ ui/src/App.js
@@ -4,7 +4,25 @@ class App extends Component {
   render() {
     return (
       <div>
-        Dashboard
+        <h2>Dashboard</h2>
+        <button>Add Process</button>
+        <div>
+          <h3>Active Processes</h3>
+          <div>
+            <div>Process 1</div>
+            <button>Remove</button>
+            <button>Edit</button>
+            <button>Reset</button>
+            <div>Visualisation here</div>
+          </div>
+          <div>
+            <div>Process 2</div>
+            <button>Remove</button>
+            <button>Edit</button>
+            <button>Reset</button>
+            <div>Visualisation here</div>
+          </div>
+        </div>
       </div>
     );
   }
